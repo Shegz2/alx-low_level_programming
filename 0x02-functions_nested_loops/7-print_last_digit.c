7ce865f37202b12c728921cb0a28cb38d4064499
shegz2 <samson.shegun736@gmail.com>
2022-10-19 07:42:05 -0700

    digit of a number.
    Prototype: int print_last_digit(int);
    Returns the value of the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - print
+ * @t: the number
+ * Return: value of the last digi
+ */
+int print_last_digit(int t)
+{
+	int l = t % 10;
+
+	if (l < 0)
+	{
+	l *= -1;
+	_putchar(l + '0');
+	return (0);
+	}
+
+}
