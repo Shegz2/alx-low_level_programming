cf0acda701d282df402008dd37f6c12f6c8c6f15
shegz2 <samson.shegun736@gmail.com>
2022-10-19 05:33:30 -0700

    completed: 0.0%) Write a function that computes the absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - compute absolutr value
+ * @t: the integer to be computed
+ * Return: absolute number
+ */
+int _abs(int t)
+{
+	if (t > 0)
+		return (t);
+	else
+		return (-t);
+}
