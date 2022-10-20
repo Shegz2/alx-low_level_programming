ee4bd6528315f63aa657287e2b231e476d62034e
shegz2 <samson.shegun736@gmail.com>
2022-10-20 16:42:41 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 9)
+	{
+		_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
