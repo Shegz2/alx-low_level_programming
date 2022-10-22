9f28d2201b0822dc48bb3a9ff11516a82ec28b03
shegz2 <samson.shegun736@gmail.com>
2022-10-21 18:18:35 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,13 @@ void more_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; i <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
+			{
 				_putchar('1');
-			_putchar(j % 10 + '0');
+				_putchar(j % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
