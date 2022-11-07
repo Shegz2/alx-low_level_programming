52a75530d6796ceab6595100533dc63ea5e22562
shegz2 <samson.shegun736@gmail.com>
2022-11-07 06:18:49 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function
+ * @argc: inputs
+ * @argv: inputs
+ * Return: Alwy 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
